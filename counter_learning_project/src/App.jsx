b4a77cec2e95ec92d00ee6c520bd07b63252bae9
shail gusain <shahilgusain777@gmail.com>
2024-11-18 21:29:31 +0530

    learning

--- counter_learning_project/src/App.jsx
+++ counter_learning_project/src/App.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react"
 function App() {
-  let [counter , setcounter] = useState(1)
-  const addvalue  = () => {
-    if(counter==20)
-    setcounter(20);
-   else
-    setcounter(counter+1)
-  }
-  function removevalue() {
-  if (counter == 0) 
-    setcounter(0);
-  else 
-    setcounter(counter-1);
   
-}
   return (
     <>
-      <h2>welcome to the counter</h2>
-      <h3>count is {counter}</h3><br />
-      <button onClick={addvalue}> add value {counter}</button><br />
-      <button onClick={removevalue}> remove value {counter}</button><br />
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand"href="/">TEXTUTILS</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page"href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link"href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search"> 
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </>
   );
 
